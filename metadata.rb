4cde03308f43a5bb4dac021da80694deba319260
Andrew Holt <andrew@theholts.org>
2015-12-03 22:29:50 +0200

    Fix the README as it doesn't display correctly in Chef

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
-name 'chef-server-backup'
-maintainer 'Andrew Holt'
+name             'chef-server-backup'
+maintainer       'Andrew Holt'
 maintainer_email 'andrew@theholts.org'
-license 'GNU Public License 3.0'
-description 'Installs/Configures backups for Chef Server'
-long_description 'Deploys and executes a backup script via CronD on the Chef Server'
-version '0.1.0'
+license          'GNU Public License 3.0'
+description      'Installs/Configures backups for Chef Server'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.1'
 
 depends 'cron', '~> 1.7.0'
